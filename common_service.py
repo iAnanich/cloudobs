5100e32ad4ae24fcfaac845433643fb7713b871b
amukhsimov <aka.mukhsimov@gmail.com>
2022-03-26 14:06:17 +0500

    error

--- common_service.py
+++ common_service.py
@@ -147,7 +147,8 @@ def set_stream_settings():
 
     # broadcast request for all lang servers
     for lang in stream_settings:
-        query_params = urlencode({lang: stream_settings[lang]})
+        params_ = {lang: stream_settings[lang]}
+        query_params = urlencode({'stream_settings': params_})
         request_ = f"{instance_service_addrs[lang]['addr']}{API_SET_STREAM_SETTINGS_ROUTE}?{query_params}"
         response = requests.post(request_)
 
